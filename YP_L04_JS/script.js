95853ea22c3f3938f86ca0035e39d883c848a35a
Anton Kuxaruk <anton.kuxaruk@mail.ru>
2019-06-27 17:53:02 +0300

    JS - task 14 - done

--- YP_L04_JS/script.js
+++ YP_L04_JS/script.js
@@ -3,4 +3,36 @@ console.log('a message...')
 document.write('Bonjour, le monde!')
 /* console.log(window)
 console.log(document) */
-console.log(Math)
+//console.log(Math)
+//console.log(innerWidth)
+console.log( Math.ceil(25.67))
+console.log( (128 + 254 + 137 + 201) / 60)
+
+var red = prompt('Введите насыщенность красного в виде числа от 0 до 255', '255')
+var green = prompt('Введите насыщенность зелёного в виде числа от 0 до 255', '255')
+var blue = prompt('Введите насыщенность синего в виде числа от 0 до 255', '255')
+
+red = Number(red)
+blue = Number(blue)
+
+if(isNaN(red)) {
+    red = prompt('В качестве значения насыщенности красного вы ввели не число. Пожалуйста, введите число от 0 до 255.', '255')
+    red = Number(red)
+}
+
+if(isNaN(green)) {
+    green = prompt('В качестве значения насыщенности зелёного вы ввели не число. Пожалуйста, введите число от 0 до 255.', '255')
+    green = Number(green)
+}
+
+if(isNaN(blue)) {
+    blue = prompt('В качестве значения насыщенности синего вы ввели не число. Пожалуйста, введите число от 0 до 255.', '255')
+    blue = Number(blue)
+}
+
+console.log(red + blue)
+console.log(red+green+blue)
+
+
+
+
